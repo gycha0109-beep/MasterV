a5aba451916e69c40fd3868fc7a1d06c6028580a
gycha0109-beep <gycha0109@gmail.com>
2026-08-20 13:39:07 +0900

    chore(exit-3): remove legacy vendor code and CI

--- supabase/migrations/202608150001_reference_library_security_hardening.sql
+++
@@ -1,5 +0,0 @@
-alter function public.masterv_is_workspace_member(text) security invoker;
-
-revoke all on function public.masterv_is_workspace_member(text) from public;
-revoke execute on function public.masterv_is_workspace_member(text) from anon;
-grant execute on function public.masterv_is_workspace_member(text) to authenticated;
